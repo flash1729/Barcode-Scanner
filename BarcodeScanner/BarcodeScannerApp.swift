1dea1b084b090f6dc3a8e1658787e92412130ccb
flash1729 <sherlockedaditya@gmail.com>
2024-11-03 11:31:46 +0530

    Add BarcodeScannerApp.swift

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Aditya Medhane on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
